2cafb30a1835ad4ab701fad1239f82040e9a1e29
RHRolun <rlundber@redhat.com>
2024-04-08 07:52:36 +0200

    03/06 and 05/05 pipelines working

--- lab-materials/03/06/test_security.py
+++ lab-materials/03/06/test_security.py
@@ -2,7 +2,7 @@ import os
 import requests
 import json
 
-def test_security(endpoint, expected_serving_sha):
+def test_security(endpoint, expected_id,):
     response = requests.get(endpoint)
 
     if response.status_code==200:
@@ -10,17 +10,17 @@ def test_security(endpoint, expected_serving_sha):
     else:
         raise Exception(f"Response status code is {response.status_code}")
 
-    if response_json["sha"] != expected_serving_sha:
-        raise Exception(f"Serving SHA has changed, endpoint may have been tampered with")
+    if expected_id not in response_json["data"][0]["permission"][0]["id"]:
+        raise Exception(f"Model ID has changed, model may have been tampered with")
 
     print(f"Security check OK")
 
     with open("security_result.json", "w") as f:
         json.dump({
-            "serving_sha_match": response_json["sha"] == expected_serving_sha
+            "model_id_match": expected_id in response_json["data"][0]["permission"][0]["id"],
         }, f)
 
 if __name__ == '__main__':
-    info_endpoint = "http://llm.ic-shared-llm.svc.cluster.local:3000" + "/info"
-    expected_serving_sha = "630800eed37b15c4b0c9eb8e6ab47212026720f7"
-    test_security(info_endpoint, expected_serving_sha)
+    info_endpoint = "http://llm.ic-shared-llm.svc.cluster.local:8000" + "/v1/models"
+    expected_id = "modelperm" #This is just for the demo, in a real scenario you would input the model id here
+    test_security(info_endpoint, expected_id)
