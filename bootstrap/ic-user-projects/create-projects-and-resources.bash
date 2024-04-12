227c680ff1e39618df4279acd589e26b87448d17
Guillaume Moutier <guillaume.moutier@gmail.com>
2024-04-12 11:32:25 -0400

    Update workbench image version to 2.0.1

--- bootstrap/ic-user-projects/create-projects-and-resources.bash
+++ bootstrap/ic-user-projects/create-projects-and-resources.bash
@@ -10,7 +10,7 @@ DASHBOARD_ROUTE=https://$(oc get route rhods-dashboard -n redhat-ods-application
 
 # Define some variables
 WORKBENCH_NAME="my-workbench"
-WORKBENCH_IMAGE="ic-workbench:2.0"
+WORKBENCH_IMAGE="ic-workbench:2.0.1"
 PIPELINE_ENGINE="Tekton"
 
 for i in $(seq 1 $user_count);
